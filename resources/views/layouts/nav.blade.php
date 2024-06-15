1053a6e949d1c58ac2ed9c8ec636b833eaf1dc98
Lucent <lucentthulung@gmail.com>
2024-06-15 11:44:16 +0545

    modified header

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,9 +1,48 @@
-<nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
+<div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h1 class="modal-title fs-5" id="loginModalLabel">Admin Login</h1>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+			<form class="modal-body">
+				<div class="mb-3">
+					<label for="loginEmail" class="form-label">Email address</label>
+					<input type="email" class="form-control" id="loginEmail" aria-describedby="emailHelp">
+				</div>
+				<div class="mb-3">
+					<label for="loginPassword" class="form-label">Password</label>
+					<input type="password" class="form-control" id="loginPassword">
+				</div>
+				<div class="mb-3 form-check">
+					<input type="checkbox" class="form-check-input" id="exampleCheck1">
+					<label class="form-check-label" for="exampleCheck1">Remember Me</label>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+					<button type="submit" class="btn btn-primary">Login</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+<nav class="navbar navbar-expand-lg navbar-light sticky-top bg-light" id="mainNav">
 	<div class="container px-4 px-lg-5">
-		<a class="navbar-brand" href="index.html">SysQube Blogs</a>
+		<a class="navbar-brand" href="/">
+			<svg xmlns="http://www.w3.org/2000/svg" id="z_logo" viewBox="0 0 64.312 40.348">
+				<g id="Group_3" data-name="Group 3" transform="translate(-90.29 -40.162)">
+				<g data-name="Group 2">
+				<path data-name="Path 1" d="M204.213,115.77l-13.348-13.348,3.354-3.356L190.971,95.8l-6.619,6.621,19.861,19.861,10.314-10.332-3.247-3.249-7.067,7.067" transform="translate(-83.604 -41.774)" fill="#fdb811" fill-rule="evenodd"></path>
+				<path data-name="Path 2" d="M234.607,71.992,221.365,58.75,211.051,69.082l3.265,3.249,7.049-7.067,7.085,7.067,2.908,2.908,3.356,3.372-3.356,3.356,3.249,3.265,6.621-6.621-6.621-6.619" transform="translate(-100.774 -17.947)" fill="#fdb811" fill-rule="evenodd"></path>
+				<path data-name="Path 3" d="M256.958,73l-5.23,5.228,2.91,2.91,2.32-2.3,8.923,8.923-8.923,8.923-2.32-2.3-3.249-3.265-3.354-3.354L251.39,84.4l-2.908-2.926L242.2,87.759l6.282,6.281,3.247,3.249,5.23,5.228,14.757-14.757L256.958,73" transform="translate(-120.805 -27.111)" fill="#003a78" fill-rule="evenodd"></path>
+				<path data-name="Path 4" d="M175.038,78.228,169.808,73,155.051,87.759l14.757,14.757,5.23-5.228-2.926-2.91-2.3,2.3-8.923-8.923,8.923-8.923,2.3,2.3,3.265,3.265,3.354,3.356-3.354,3.354,2.908,2.926,6.282-6.281-6.282-6.282-3.247-3.249" transform="translate(-64.761 -27.111)" fill="#003a78" fill-rule="evenodd"></path>
+				</g>
+				</g>
+			</svg>
+			<h3 class="d-inline-block">SysQube Blogs</h3>
+		</a>
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-			Menu
-			<i class="fas fa-bars"></i>
+			<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M7.95 11.95h32m-32 12h32m-32 12h32"/></svg>
 		</button>
 		<div class="collapse navbar-collapse" id="navbarResponsive">
 			<ul class="navbar-nav ms-auto py-4 py-lg-0">
@@ -13,34 +52,6 @@
 				<li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="/about">About</a></li>
 				<li class="nav-item d-flex align-items-center"><button type="button" class="btn btn-primary p-2"  data-bs-toggle="modal" data-bs-target="#loginModal">Login</button></li>
 			</ul>
-		</div>
-		<div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<h1 class="modal-title fs-5" id="loginModalLabel">Admin Login</h1>
-						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-					</div>
-					<form class="modal-body">
-						<div class="mb-3">
-							<label for="loginEmail" class="form-label">Email address</label>
-							<input type="email" class="form-control" id="loginEmail" aria-describedby="emailHelp">
-						</div>
-						<div class="mb-3">
-							<label for="loginPassword" class="form-label">Password</label>
-							<input type="password" class="form-control" id="loginPassword">
-						</div>
-						<div class="mb-3 form-check">
-							<input type="checkbox" class="form-check-input" id="exampleCheck1">
-							<label class="form-check-label" for="exampleCheck1">Remember Me</label>
-						</div>
-						<div class="modal-footer">
-							<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-							<button type="submit" class="btn btn-primary">Login</button>
-						</div>
-					</form>
-				</div>
-			</div>
 		</div>
 	</div>
 </nav>
