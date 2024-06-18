47ca9489e342b8e600b06e7b1c7496ba44384e40
Lucent <lucentthulung@gmail.com>
2024-06-18 22:33:18 +0545

    replace login button with dashboard link when logged in

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,3 +1,4 @@
+@if(!Auth::check())
 <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
 	<div class="modal-dialog">
 		<div class="modal-content">
@@ -29,6 +30,7 @@
 		</div>
 	</div>
 </div>
+@endif
 <nav class="navbar navbar-expand-lg navbar-light sticky-top bg-light" id="mainNav">
 	<div class="container px-4 px-lg-5">
 		<a class="navbar-brand" href="/">
@@ -45,10 +47,16 @@
 				<li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="/contact">Contact</a></li>
 				<li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="/about">About</a></li>
 				<li class="nav-item d-flex align-items-center">
-					<button type="button" class="btn btn-primary p-2"  data-bs-toggle="modal" data-bs-target="#loginModal">
-						Login
-						<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M10.3 7.7a.984.984 0 0 0 0 1.4l1.9 1.9H3c-.55 0-1 .45-1 1s.45 1 1 1h9.2l-1.9 1.9a.984.984 0 0 0 0 1.4c.39.39 1.01.39 1.4 0l3.59-3.59a.996.996 0 0 0 0-1.41L11.7 7.7a.984.984 0 0 0-1.4 0M20 19h-7c-.55 0-1 .45-1 1s.45 1 1 1h7c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2h-7c-.55 0-1 .45-1 1s.45 1 1 1h7z"/></svg>
-					</button>
+					@if(!Auth::check())
+						<button type="button" class="btn btn-primary p-2"  data-bs-toggle="modal" data-bs-target="#loginModal">
+							Login
+							<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M10.3 7.7a.984.984 0 0 0 0 1.4l1.9 1.9H3c-.55 0-1 .45-1 1s.45 1 1 1h9.2l-1.9 1.9a.984.984 0 0 0 0 1.4c.39.39 1.01.39 1.4 0l3.59-3.59a.996.996 0 0 0 0-1.41L11.7 7.7a.984.984 0 0 0-1.4 0M20 19h-7c-.55 0-1 .45-1 1s.45 1 1 1h7c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2h-7c-.55 0-1 .45-1 1s.45 1 1 1h7z"/></svg>
+						</button>
+					@else
+						<a class="btn btn-primary p-2" href="/dashboard">
+							Dashboard
+						</a>
+					@endif
 				</li>
 			</ul>
 		</div>
