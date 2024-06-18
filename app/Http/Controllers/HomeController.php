6308f8b6507b17ae49925a4383779adae85ee9d6
Lucent <lucentthulung@gmail.com>
2024-06-18 22:20:03 +0545

    show only published posts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
 	public function home(){
-		$posts = Post::orderBy('created_at', 'desc')->take(5)->get();
+		$posts = Post::where('status', 'published')->orderBy('created_at', 'desc')->take(5)->get();
 		return view('home.index')->with('posts', $posts);
 	}
 
